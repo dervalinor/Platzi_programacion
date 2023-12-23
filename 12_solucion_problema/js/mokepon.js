ea9cdf51d0f224a43e95ae908eededeb6a9f3f3f
dervalinor <dervalinor@protonmail.com>
2023-12-22 23:08:33 -0500

    Tarea primera parte resolvida

---
+++ 12_solucion_problema/js/mokepon.js
@@ -0,0 +1,294 @@
+const sectionSeleccionarAtaque = document.getElementById('seleccionar-ataque')
+const sectionReiniciar = document.getElementById('reiniciar')
+const botonMascotaJugador = document.getElementById('boton-mascota')
+const botonTierra = document.getElementById('boton-tierra')
+sectionReiniciar.style.display = 'none'
+const botonFuego = document.getElementById('boton-fuego')
+const botonAgua = document.getElementById('boton-agua')
+const botonReiniciar = document.getElementById('boton-reiniciar')
+
+const sectionSeleccionarMascota = document.getElementById('seleccionar-mascota')
+/*Recordar que esto id de personajes ya no existen ya que se remplazo por medio
+  * OOP por lo cual da este error en el codigo ya que se dirige a elementos que
+  * ya no existen, entonce para evitar este error las cambiamos de posicion
+  * y les quitamos const por let ya que cambian de valor por los personajes
+  * creados*/
+/*
+
+const inputHipodoge = document.getElementById('hipodoge')
+const inputCapipepo = document.getElementById('capipepo')
+const inputRatigueya = document.getElementById('ratigueya')
+
+*/
+const spanMascotaJugador = document.getElementById('mascota-jugador')
+
+const spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+
+const spanVidasJugador = document.getElementById('vidas-jugador')
+const spanVidasEnemigo = document.getElementById('vidas-enemigo')
+
+const sectionMensajes = document.getElementById('resultado')
+const ataquesDelJugador = document.getElementById('ataques-del-jugador')
+const ataquesDelEnemigo = document.getElementById('ataques-del-enemigo')
+const contenedorTarjetas = document.getElementById('contenedorTarjetas') 
+
+let mokepones = []
+let ataqueJugador
+let ataqueEnemigo
+let opcionDeMokepones
+let vidasJugador = 3
+let vidasEnemigo = 3
+/*les quitamos el valor inicial y la dejamos como variables sin valor */
+let inputHipodoge
+let inputCapipepo
+let inputRatigueya
+
+
+class Mokepon {
+    constructor(nombre, foto, vida) {
+        this.nombre = nombre
+        this.foto = foto
+        this.vida = vida
+        this.ataques = []
+    }
+}
+
+let hipodoge = new Mokepon('Hipodoge', 'https://i.imgur.com/F6IZstF.jpg', 5)
+
+let capipepo = new Mokepon('Capipepo', 'https://i.imgur.com/6rdQP6l.jpg', 5)
+
+let ratigueya = new Mokepon('Ratigueya', 'https://i.imgur.com/oioTlmZ.jpg', 5)
+
+hipodoge.ataques.push(
+    { nombre: '💧', id: 'boton-agua' },
+    { nombre: '💧', id: 'boton-agua' },
+    { nombre: '💧', id: 'boton-agua' },
+    { nombre: '🔥', id: 'boton-fuego' },
+    { nombre: '🌱', id: 'boton-tierra' },
+)
+
+capipepo.ataques.push(
+    { nombre: '🌱', id: 'boton-tierra' },
+    { nombre: '🌱', id: 'boton-tierra' },
+    { nombre: '🌱', id: 'boton-tierra' },
+    { nombre: '💧', id: 'boton-agua' },
+    { nombre: '🔥', id: 'boton-fuego' },
+    
+)
+
+ratigueya.ataques.push(
+    { nombre: '🔥', id: 'boton-fuego' },
+    { nombre: '🔥', id: 'boton-fuego' },
+    { nombre: '🔥', id: 'boton-fuego' },
+    { nombre: '💧', id: 'boton-agua' },
+    { nombre: '🌱', id: 'boton-tierra' },
+)
+
+
+mokepones.push(hipodoge, capipepo, ratigueya)
+
+function iniciarJuego() {
+    
+    sectionSeleccionarAtaque.style.display = 'none'
+
+    /*ocurre que este codigo da el error de mokepon.js:123 Uncaught TypeError: Cannot read properties of null (reading 'checked')
+    at HTMLButtonElement.seleccionarMascotaJugador (mokepon.js:123:23)
+seleccionarMascotaJugador @ mokepon.js:123, por lo cual al seleccionar el los poderes no aparece el nombre del personaje */
+    mokepones.forEach((mokepon) => {
+	    opcionDeMokepones = ` 
+	              <input type="radio" name="mascota" id=${mokepon.nombre} />
+                <label class="tarjeta-de-mokepon" for=${mokepon.nombre} >
+
+                  <div class="personaje-container">
+                      <p class="titulo-personaje">${mokepon.nombre} </p>
+                      <!--Agregar imagenes de los personajes -->
+                      <img src=${mokepon.foto} alt=${mokepon.nombre} >
+
+                  </div>  
+
+	    `
+      contenedorTarjetas.innerHTML += opcionDeMokepones 
+      /*ahora le damos valor a la variables de los personajes un vez injectadas
+        * los objetos y metodos en html, ES IMPORTANTE: Que el id debe
+        * coindicir con la propiedad id que se inyecta como es en el casdo
+        * mokepon.name */
+       inputHipodoge = document.getElementById('Hipodoge')
+       inputCapipepo = document.getElementById('Capipepo')
+       inputRatigueya = document.getElementById('Ratigueya')
+
+    })
+  //importante!!!: vamos a manejar la informacion dada por nuestros objetos
+  //estos sera nuestra unica fuente y asi ahorrarnos codigo en html por medio
+  //de esto    
+
+
+    
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+    
+    botonFuego.addEventListener('click', ataqueFuego)
+    
+    botonAgua.addEventListener('click', ataqueAgua)
+    
+    botonTierra.addEventListener('click', ataqueTierra)
+
+    
+    botonReiniciar.addEventListener('click', reiniciarJuego)
+}
+
+function seleccionarMascotaJugador() {
+    
+    sectionSeleccionarMascota.style.display = 'none'
+    
+    
+    sectionSeleccionarAtaque.style.display = 'flex'
+    
+   //hacer la informacion dinamica 
+   //aqui hay informacion escrita a mano sobre los nombres de los personajes
+  //esto puede ser remplazado por los metodos de los obejetos para mostrar los
+  //atributos
+  //Nota: typeof(elemento) nos dice que tipo de elemento es. 
+  //si ponemos inputCapipepo.id obtenemos el id de ese elemento si es un objeto
+    if (inputHipodoge.checked) {
+        spanMascotaJugador.innerHTML = inputHipodoge.id
+    } else if (inputCapipepo.checked) {
+        spanMascotaJugador.innerHTML = inputCapipepo.id
+    } else if (inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = inputRatigueya.id
+    } else {
+        alert('Selecciona una mascota')
+    }
+
+    seleccionarMascotaEnemigo()
+}
+
+//en esta funcion podemos utilizar OOP parea evitar escribir elementos a mano
+//y solo use la informacion automatica de nuestros objetos
+function seleccionarMascotaEnemigo() {
+  //como el numero de mascotas en variable usamos la propiedad de longitud del
+  //objeto para la seleccion aleatoria
+    let mascotaAleatoria = aleatorio(0, mokepones.length - 1) //ya que comienza desde el indice cero
+    spanMascotaEnemigo.innerHTML = mokepones[mascotaAleatoria].nombre //nombre del mokepon en un determinado indice
+  //esto reduce mucho el codigo
+  //innerHTML inyectar codigo html
+
+ }
+
+//Tarea
+//hay un problema en el codigo html es que tenemos solo tres botones pero los
+//personajes pueden tienen mas esto se soluciona usando propiedades de los
+//objetos
+
+function ataqueFuego() {
+  //esta linea consiste en ataques escritos, debe ser
+  //replazodo por el metodo de ataques de los objetos
+  //Recorda que los ataques esta en lista, hay que recorrerla y colocar
+  //cada ataque, creo que se debe colocar un ciclo
+    ataqueJugador = 'FUEGO'
+    ataqueAleatorioEnemigo()
+}
+function ataqueAgua() {
+    ataqueJugador = 'AGUA'
+    ataqueAleatorioEnemigo()
+}
+function ataqueTierra() {
+    ataqueJugador = 'TIERRA'
+    ataqueAleatorioEnemigo()
+}
+
+function ataqueAleatorioEnemigo() {
+    let ataqueAleatorio = aleatorio(0, mokepones.length - 1)
+    
+    //crear un ciclo donde se compare el valor de la variable
+   //con el indice del poder de mokepon
+    
+   for (var i = 0; i < mokepones.length; i++){
+    if(ataqueEnemigo == mokepones[i].nombre){
+      ataqueEnemigo = mokepones[i].nombre
+    }
+   }
+
+    /*if (ataqueAleatorio == 1) {
+        ataqueEnemigo = 'FUEGO'
+    } else if (ataqueAleatorio == 2) {
+        ataqueEnemigo = 'AGUA'
+    } else {
+        ataqueEnemigo = 'TIERRA'
+    }*/
+
+    combate()
+}
+
+function combate() {
+    
+   //Agregar OOP a este codigo 
+    if(ataqueEnemigo == ataqueJugador) {
+        crearMensaje("EMPATE")
+    } else if(ataqueJugador == 'FUEGO' && ataqueEnemigo == 'TIERRA') {
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+    } else if(ataqueJugador == 'AGUA' && ataqueEnemigo == 'FUEGO') {
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+    } else if(ataqueJugador == 'TIERRA' && ataqueEnemigo == 'AGUA') {
+        crearMensaje("GANASTE")
+        vidasEnemigo--
+        spanVidasEnemigo.innerHTML = vidasEnemigo
+    } else {
+        crearMensaje("PERDISTE")
+        vidasJugador--
+        spanVidasJugador.innerHTML = vidasJugador
+    }
+
+    revisarVidas()
+}
+
+function revisarVidas() {
+    if (vidasEnemigo == 0) {
+        crearMensajeFinal("FELICITACIONES! Ganaste :)")
+    } else if (vidasJugador == 0) {
+        crearMensajeFinal('Lo siento, perdiste :(')
+    }
+}
+
+function crearMensaje(resultado) {
+    
+    
+    let nuevoAtaqueDelJugador = document.createElement('p')
+    let nuevoAtaqueDelEnemigo = document.createElement('p')
+
+    sectionMensajes.innerHTML = resultado
+    nuevoAtaqueDelJugador.innerHTML = ataqueJugador
+    nuevoAtaqueDelEnemigo.innerHTML = ataqueEnemigo
+
+    ataquesDelJugador.appendChild(nuevoAtaqueDelJugador)
+    ataquesDelEnemigo.appendChild(nuevoAtaqueDelEnemigo)
+}
+
+function crearMensajeFinal(resultadoFinal) {
+    
+    
+    sectionMensajes.innerHTML = resultadoFinal
+
+    
+    botonFuego.disabled = true
+    
+    botonAgua.disabled = true
+    
+    botonTierra.disabled = true
+
+    
+    sectionReiniciar.style.display = 'block'
+}
+
+function reiniciarJuego() {
+    location.reload()
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+window.addEventListener('load', iniciarJuego)
